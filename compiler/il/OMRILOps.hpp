d7af136bb3e48c0af9aa57aeba0a7a8803281bcc
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2019-07-24 14:27:40 -0400

    Test: remove cstorei in OMRILOps.hpp
    Signed-off-by: Bohao(Aaron) Wang <aaronwang0407@gmail.com>

--- compiler/il/OMRILOps.hpp
+++ compiler/il/OMRILOps.hpp
@@ -1284,7 +1284,6 @@ public:
          case TR::dstore:
             return TR::vstore;
          case TR::bstorei:
-         case TR::cstorei:
          case TR::sstorei:
          case TR::istorei:
          case TR::lstorei:
